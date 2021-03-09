3a354487b12d05cf93f858a1555a4fb52acaa6ad
Yudi <singh.yudi10@gmail.com>
2021-03-09 22:30:47 +0530

    added passport and jwt auth

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const express = require("express");
+require("./login");
+const auth = require("./routes/auth");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
-
-app.use(logger('dev'));
+const app = express();
+app.use("/auth", auth);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  usersRouter
+);
 
 module.exports = app;
